7820720745a706fa2539f375d4127ad36e954e66
away7000 <83220445+away7000@users.noreply.github.com>
2026-04-13 21:51:22 +0700

    Create seadrop.js

---
+++ src/abi/seadrop.js
@@ -0,0 +1,5 @@
+export const SEADROP_ABI = [
+  "function mintPublic(address nftContract,address minter,uint256 quantity)",
+  "function mintAllowed(address nftContract,address minter,uint256 quantity)",
+  "function mintSigned(address nftContract,address minter,uint256 quantity)"
+];
